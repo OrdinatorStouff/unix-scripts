0ef3af7b1e87859a2ddad2c02a49eea83b59fa3c
OrdinatorStouff <Josh.Stouffer@AZDCS.GOV>
2019-11-18 19:12:36 -0700

    adding more

---
+++ install-common-software.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#
+# Must-have packages 
+#
+#
+# mostly for my own record-keeping
+# not meant to be an objective reference
+#
+# WORK IN PROGRESS
+
+
+
+# https://www.gnu.org/software/bc/
+# CLI floating-point math 
+    sudo apt-get install bc
+
+
+
+
+sudo apt-get install bash
+sudo apt-get install git
+sudo apt-get install grep
+sudo apt-get install awk
+sudo apt-get install nano # or nano-tiny if necessary
+
+# look into busybox
+
+
+
+# PGP keys
+sudo apt-get install gnupg 
+
+
+
+# TODO logic to check for different package managers
+
+# pacman -S 
+# yum install
+# apt install
+# apt-get install
+# dnf install
